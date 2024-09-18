a7511e2f51ec2e5afa0ea9f9a6fd10fd4bc813c9
yEs1do <yangjiaxiyou@gmail.com>
2024-09-18 16:38:45 +0800

    fix

--- tasks/base_task.py
+++ tasks/base_task.py
@@ -75,7 +75,7 @@ class BaseTask(GlobalGameAssets, CostumeBase):
             return False
         logger.info('Invitation appearing')
         invite_type = self.config.global_game.emergency.friend_invitation
-        when_accept_invitation = self.config.global_game.emergency.when_accept_invitation
+        accept_then = self.config.global_game.emergency.accept_invitation_complete_now
 
         detect_record = self.device.detect_record
         match invite_type:
@@ -117,7 +117,7 @@ class BaseTask(GlobalGameAssets, CostumeBase):
         # 有的时候长战斗 点击后会取消战斗状态
         self.device.detect_record = detect_record
         # 如果接受邀请则立即执行悬赏任务
-        if self.config.global_game.emergency.accept_invitation_complete_now and click_button == self.I_G_ACCEPT:
+        if accept_then and click_button == self.I_G_ACCEPT:
             self.set_next_run(task='WantedQuests', target=datetime.now())
         return True
 
