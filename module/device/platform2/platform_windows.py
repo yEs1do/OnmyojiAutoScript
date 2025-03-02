005f93f82ba1840e9ebc3905b00dc0477a3e95eb
yEs1do <yangjiaxiyou@gmail.com>
2025-03-02 23:09:46 +0800

    增加模拟器启动后最小化选项

--- module/device/platform2/platform_windows.py
+++ module/device/platform2/platform_windows.py
@@ -315,7 +315,8 @@ class PlatformWindows(PlatformBase, EmulatorManager):
             # All check passed
             break
 
-        if False:
+        emulator_window_minimize = self.config.script.device.emulator_window_minimize
+        if emulator_window_minimize:
             if new_window != 0 and new_window != current_window:
                 # 如果有新窗口且新窗口不是当前窗口，最小化新窗口
                 logger.info(f'Minimize new window: {new_window}')
