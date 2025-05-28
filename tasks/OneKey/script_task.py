2466508ad16ef2efbd27dc7276bd538f6e5524ac
yEs1do <yangjiaxiyou@gmail.com>
2025-05-28 21:18:52 +0800

    优化递归逻辑，截取图片

--- tasks/OneKey/script_task.py
+++ tasks/OneKey/script_task.py
@@ -16,87 +16,61 @@ from tasks.OneKey.config import ScrollNumber
 
 class ScriptTask(GameUi, OneKeyAssets):
 
-    def run(self): 
-        # self.ui_get_current_page()
-        # self.ui_goto(page_summon)
+    def run(self):
         con = self.config.one_key.one_key_config
-        # 计次循环
+        # 构造资源列表（根据最大页数，动态截取）
+        max_page = con.page_number
+        ok_images = [self.I_OK_1, self.I_OK_2, self.I_OK_3]
+        ok_clicks = [self.C_OK_1, self.C_OK_2, self.C_OK_3]
+        timeouts = [con.time_1, con.time_2, con.time_3]
+        # 只取前 max_page 项
+        self.ok_images = ok_images[:max_page]
+        self.ok_clicks = ok_clicks[:max_page]
+        self.timeouts = timeouts[:max_page]
+
         for i in range(con.times):
             logger.info(f'OneKey task round {i+1}/{con.times}')
-            self.one_key_1(con)
+            self.one_key_page(con, page=0)  # 用 0-based 索引
         logger.info('Task over')
         self.goto_page_main(con)
         self.set_next_run(task='OneKey', success=True)
         raise TaskEnd
 
-    def one_key_1(self, con):
-        if self.wait_until_appear(self.I_OK_1, wait_time=999):
-            logger.info('Appear page 1')
-            self.click(self.C_OK_1, interval=1)
-            sleep(con.time_1)
-            if con.page_number == 1:
-                logger.info('Page number is 1, return')
-                return
-            # 如果页面数量大于1，则继续进入下一页
-            while 1:
-                self.screenshot()
-                if self.appear(self.I_OK_2):
-                    break
-                if self.appear_then_click(self.I_OVER_GHOST, interval=1):
-                    continue
-            logger.info('Going to page 2')
-            self.one_key_2(con)
-        else:
-            logger.error('Failed to enter page 1')
+    def one_key_page(self, con, page: int):
+        """
+        递归处理第 page 页（0-based）。
+        """
+        max_page = len(self.ok_images)
+        I_current = self.ok_images[page]
+        C_current = self.ok_clicks[page]
+        sleep_time = self.timeouts[page]
+
+        # 等待当前页
+        if not self.wait_until_appear(I_current, wait_time=999):
+            logger.error(f'Failed to enter page {page+1}')
             self.set_next_run(task='OneKey', success=False)
             raise TaskEnd
-    
-    def one_key_2(self, con):
-        if self.wait_until_appear(self.I_OK_2, wait_time=999):
-            logger.info('Appear page 2')
-            self.click(self.C_OK_2, interval=1)
-            sleep(con.time_2)
-            if con.page_number == 2:
-                logger.info('Page number is 2, return')
-                return
-            # 如果页面数量大于2，则继续进入下一页
-            while 1:
-                self.screenshot()
-                if self.appear(self.I_OK_3):
-                    break
-                if self.appear_then_click(self.I_OVER_GHOST, interval=1):
-                    continue
-            logger.info('Going to page 3')
-            self.one_key_3(con)
-        else:
-            logger.error('Failed to enter page 2')
-            self.set_next_run(task='OneKey', success=False)
-            raise TaskEnd
-        
-    def one_key_3(self, con):
-        if self.wait_until_appear(self.I_OK_3, wait_time=999):
-            logger.info('Appear page 3')
-            self.click(self.C_OK_3, interval=1)
-            sleep(con.time_3)
-            if con.page_number == 3:
-                logger.info('Page number is 3, return')
-                return
-            # 如果页面数量大于3，则继续进入下一页
-            while 1:
-                self.screenshot()
-                if self.appear(self.I_OK_4):
-                    break
-                if self.appear_then_click(self.I_OVER_GHOST, interval=1):
-                    continue
-            logger.info('Going to page 4')
-            self.one_key_4(con)
-        else:
-            logger.error('Failed to enter page 3')
-            self.set_next_run(task='OneKey', success=False)
-            raise TaskEnd
-    
-    def one_key_4(self, con):
-        pass
+
+        logger.info(f'Appear page {page+1}')
+        self.click(C_current, interval=1)
+        sleep(sleep_time)
+
+        # 到达最后一页则返回
+        if page == max_page - 1:
+            logger.info(f'Page number is {page+1}, return')
+            return
+
+        # 否则等待下页出现或御魂溢出，然后递归
+        next_image = self.ok_images[page+1]
+        while 1:
+            self.screenshot()
+            if self.appear(next_image):
+                break
+            if self.appear_then_click(self.I_OVER_GHOST, interval=1):
+                continue
+
+        logger.info(f'Going to page {page+2}')
+        self.one_key_page(con, page=page+1)
     
     def goto_page_main(self, con):
         pass
