82e1638e5c76288317bc4e88edef9aad0402f5bb
magicemptycity <2214463059@qq.com>
2026-05-25 02:10:27 +0800

    feat(WeeklyTrifles): 更改式神图鉴分享方式 - 通过点击式神绘卷再分享方式改为通过直接点击分享按钮

--- tasks/WeeklyTrifles/page.py
+++ tasks/WeeklyTrifles/page.py
@@ -4,14 +4,14 @@ from tasks.GameUi.page_definition import Page
 from tasks.GlobalGame.assets import GlobalGameAssets
 from tasks.WeeklyTrifles.assets import WeeklyTriflesAssets
 
-page_shikigami_collection = Page(WeeklyTriflesAssets.I_WT_SCROLL)
+page_shikigami_collection = Page(WeeklyTriflesAssets.I_WT_SHARE)
 page_collection.connect(page_shikigami_collection, GameUiAssets.I_CHECK_COLLECTION,
                                   key="page_collection->page_shikigami_collection")
 page_shikigami_collection.connect(page_collection, GlobalGameAssets.I_UI_BACK_YELLOW,
                                   key="page_shikigami_collection->page_collection")
 
-page_shikigami_scroll = Page(WeeklyTriflesAssets.I_WT_COLLECT)
-page_shikigami_collection.connect(page_shikigami_scroll, WeeklyTriflesAssets.I_WT_SCROLL,
-                                  key="page_shikigami_collection->page_shikigami_scroll")
-page_shikigami_scroll.connect(page_shikigami_collection, GlobalGameAssets.I_UI_BACK_BLUE,
-                              key="page_shikigami_scroll->page_shikigami_collection")
+page_shikigami_share = Page(WeeklyTriflesAssets.I_WT_COLLECT_WECHAT)
+page_shikigami_collection.connect(page_shikigami_share, WeeklyTriflesAssets.I_WT_SHARE,
+                                  key="page_shikigami_collection->page_shikigami_share")
+page_shikigami_share.connect(page_shikigami_collection, GlobalGameAssets.I_UI_BACK_RED,
+                              key="page_shikigami_share->page_shikigami_collection")
