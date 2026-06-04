a35203a27a253e026142b60697ce2e54acbc8450
magicemptycity <2214463059@qq.com>
2026-06-04 14:53:10 +0800

    feat(DailyTrifles): 添加右上角邮件图标识别规则及相关资源 -方便开了月度运势礼包的玩家

--- tasks/DailyTrifles/assets.py
+++ tasks/DailyTrifles/assets.py
@@ -34,6 +34,8 @@ class DailyTriflesAssets:
 	I_READ_ALL_MAIL = RuleImage(roi_front=(180,605,130,35), roi_back=(149,588,182,72), threshold=0.8, method="Template matching", file="./tasks/DailyTrifles/dt/dt_read_all_mail.png")
 	# 确认收取邮件 
 	I_HARVEST_MAIL_CONFIRM = RuleImage(roi_front=(687,543,168,64), roi_back=(687,543,168,64), threshold=0.8, method="Template matching", file="./tasks/DailyTrifles/dt/dt_harvest_mail_confirm.png")
+	# 右上角的邮件图标 
+	I_DT_HARVEST_MAIL_COPY2 = RuleImage(roi_front=(1158,18,22,20), roi_back=(1127,0,61,56), threshold=0.9, method="Template matching", file="./tasks/DailyTrifles/dt/dt_harvest_mail_copy2.png")
 
 
 	# Image Rule Assets
